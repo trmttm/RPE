5899b478b93828acae3fea822fc6d7d2f367889b
trmttm <96355189+trmttm@users.noreply.github.com>
2021-12-27 21:27:45 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rpe_to_normal',
+    version='1.0.0',
+    packages=['rpe_to_normal'],
+    url='',
+    license='',
+    author='Taro Yamaka',
+    author_email='t.yamaka.1213@gmail.com',
+    description=''
+)
